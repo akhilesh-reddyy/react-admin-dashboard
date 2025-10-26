e801ad079db445b587c9548d4cdaec581285aefc
akhilesh-reddyy <138716472+akhilesh-reddyy@users.noreply.github.com>
2025-10-26 14:40:15 +0530

    Update calendar.jsx

--- src/scenes/calendar/calendar.jsx
+++ src/scenes/calendar/calendar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import FullCalendar from '@fullcalendar/react';
-import formatDate from '@fullcalendar/core/locales-all';
+import { formatDate } from "@fullcalendar/core/internal";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
